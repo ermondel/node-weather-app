e9b3db6d3e6bfdefb9ffe78fa914abc7d507ca33
ermondel <ermondel@gmail.com>
2018-11-01 20:57:22 +0200

    test: Add output of address, latitude and longitude to console

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyDirdpH1r56LgeKV1JNH0TXXxuf7LUW_H4',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
