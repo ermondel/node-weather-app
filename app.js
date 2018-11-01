7fa8f70264df089f463c54a2d62e4b98366f6f5e
ermondel <ermondel@gmail.com>
2018-11-01 20:28:30 +0200

    feat: Add Google Maps API Key

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyDirdpH1r56LgeKV1JNH0TXXxuf7LUW_H4',
     json: true
 }, (error, response, body) => {
     console.log(body);
