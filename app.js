a3583da4d5a1858e57342c178211aeaa608eacd3
ermondel <ermondel@gmail.com>
2018-11-03 11:00:16 +0200

    feat: Add CLI command -a (address for weather forecast) and add basic help

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true,
+        },
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyDirdpH1r56LgeKV1JNH0TXXxuf7LUW_H4',
+    url: null,
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`);
