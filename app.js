d0d9030216c5376951efa7c491475b2da26eb53f
ermondel <ermondel@gmail.com>
2018-11-05 20:46:01 +0200

    feat: Add error handling of request to googleapis maps geocode

--- app.js
+++ app.js
@@ -21,7 +21,15 @@ request({
     url: requestURL,
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    if (error) {
+        console.log('Unable to connect to Google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address.');
+    } else if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    } else {
+        console.log('Unknown response from Google servers.');
+    }
 });
