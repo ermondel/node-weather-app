a2fab4d53add9e5cb54b12831fe09fec08397924
ermondel <ermondel@gmail.com>
2018-11-13 15:43:36 +0200

    feat: Add latitude, longitude and callback (for result) as arguments to the Weather module

--- weather/weather.js
+++ weather/weather.js
@@ -1,27 +1,30 @@
 const request = require('request');
 
-const getWeather = () => {
+const getWeather = (lat, lng, callback) => {
     const key = '';
 
     request({
-        url: `https://api.darksky.net/forecast/${key}/39.9396284,-75.18663959999999`,
+        url: `https://api.darksky.net/forecast/${key}/${lat},${lng}`,
         json: true
     }, (error, response, body) => {
         if (error) {
     
-            console.log('Unable to connect to Darksky.net server.');
+            callback('Unable to connect to Darksky.net server.');
     
         } else if (response.statusCode === 400) {
     
-            console.log('The given location is invalid.');
+            callback('The given location is invalid.');
     
         } else if (response.statusCode === 200) {
     
-            console.log(body.currently.temperature);
+            callback(undefined, {
+                temperature: body.currently.temperature,
+                apparentTemperature: body.currently.apparentTemperature
+            });
     
         } else {
     
-            console.log('Unknown response from Darksky.net server.');
+            callback('Unknown response from Darksky.net server.');
     
         }
     }); 
