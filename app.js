00bedb81018bdff65f8dfb5611bf63d0c2ed3e49
ermondel <ermondel@gmail.com>
2018-11-13 15:48:45 +0200

    feat: Connect the Weather module to the app and run the module in test mode

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
 const argv = yargs
     .options({
@@ -13,11 +14,19 @@ const argv = yargs
     .help()
     .alias('help', 'h')
     .argv;
-
+/*
 geocode.geocodeAddress(argv.address, (errorMessage, results) => {
     if (errorMessage) {
         console.log(errorMessage);
     } else {
         console.log(JSON.stringify(results, undefined, 2));
+    }
+});
+*/
+weather.getWeather(39.9396284, -75.18663959999999, (errorMessage, weatherResults) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(weatherResults, undefined, 2));
     }
 });
