da36ce8ac555a89cd766bba2fec6f6ffbe61456c
Ermondel <ermondel@gmail.com>
2019-09-25 23:23:22 +0300

    feat: module for obtaining the geocode of a specified location

--- playground/app.js
+++ playground/app.js
@@ -1,15 +1,27 @@
 const request = require('request');
+const geocode = require('./geocode');
 
 const url = `https://api.darksky.net/forecast/${process.env.API_KEY}/37.8267,-122.4233?units=si`;
 
 request({ url, json: true }, (error, response) => {
-  const forecast = {
-    temp: response.body.currently.temperature,
-    rain: response.body.currently.precipProbability,
-    firstDay: response.body.daily.data[0].summary
-  };
+  if (error) {
+    console.log('Unable to connect to weather service.');
+  } else if (response.body.error) {
+    console.log('Unable to find location.');
+  } else {
+    const forecast = {
+      temp: response.body.currently.temperature,
+      rain: response.body.currently.precipProbability,
+      firstDay: response.body.daily.data[0].summary
+    };
 
-  console.log(
-    `${forecast.firstDay} It is currently ${forecast.temp} degrees out. There is a ${forecast.rain}% chance of rain.`
-  );
+    console.log(
+      `${forecast.firstDay} It is currently ${forecast.temp} degrees out. There is a ${forecast.rain}% chance of rain.`
+    );
+  }
+});
+
+geocode('Boston', (error, data) => {
+  console.log('Error', error);
+  console.log(data);
 });
