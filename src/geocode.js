fd8d65d74fef1e3244b28e09588a8bbd8fb83d8a
Ermondel <ermondel@gmail.com>
2019-11-20 11:54:11 +0200

    chore: remove old code, change version and some options

--- src/geocode.js
+++ src/geocode.js
@@ -3,7 +3,7 @@ const request = require('request');
 let url = `https://api.mapbox.com/geocoding/v5/mapbox.places/{address}.json?access_token={key}&limit=1`;
 const errors = {
   connect: 'Unable to connect to location service.',
-  location: 'Unable to find location. Try another search.'
+  location: 'Unable to find location. Try another search.',
 };
 
 const geocode = (address, callback) => {
@@ -19,7 +19,7 @@ const geocode = (address, callback) => {
       const data = {
         longitude: response.body.features[0].center[0],
         latitude: response.body.features[0].center[1],
-        location: response.body.features[0].place_name
+        location: response.body.features[0].place_name,
       };
 
       callback(undefined, data);
